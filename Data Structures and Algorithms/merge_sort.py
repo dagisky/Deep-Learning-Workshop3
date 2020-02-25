a456e8e406b7a04f48e1ac06ef934d74ff4cce3f
unknown <dagisky@gmail.com>
2020-02-25 21:26:40 +0800

    insertion sort added and merge sort modified

--- Data Structures and Algorithms/merge_sort.py
+++ Data Structures and Algorithms/merge_sort.py
@@ -6,9 +6,9 @@ def mergesort(numlist):
 	numlist = list(numlist)
 	if len(numlist) > 1:
 		l, r = numlist[len(numlist)//2:], numlist[:len(numlist)//2]
-		l = mergesort(l)
-		r = mergesort(r)
-		i,j,k = 0,0,0
+		l = mergesort(l) # left section 
+		r = mergesort(r) # right section
+		i,j,k = 0,0,0 # index for left, right and whole section of the input array
 
 		while i < len(l) and j < len(r):
 			if l[i] <= r[j]:
@@ -18,6 +18,10 @@ def mergesort(numlist):
 				numlist[k] = r[j]
 				j += 1
 			k += 1
+		"""
+		# add the remaining values of the left and right section 
+		# of the merge sort
+		"""
 		while i < len(l):
 			numlist[k] = l[i]
 			i += 1
