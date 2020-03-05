555ae82b2eec2533c5f754b1239540acb9fce232
unknown <dagisky@gmail.com>
2020-03-05 21:28:41 +0800

    bubble sort python added

--- Data Structures and Algorithms/bubble_sort.py
+++ Data Structures and Algorithms/bubble_sort.py
@@ -1,1 +1,23 @@
 import numpy as np
+
+unsorted = np.random.randint(20, size=10)
+print(unsorted)
+
+
+def bubble_sort(num_list):
+	n = len(num_list)
+	# Traverse through all array elements
+	for i in range(n):
+		# The last elements are already in place
+		for j in range(0, n-i-1):
+			# Traverse the array from 0 to n-i-1
+			# Swap if the element is found greater than the 
+			# next element
+			if num_list[j] > num_list[j+1]:
+				num_list[j], num_list[j+1] = num_list[j+1], num_list[j]
+	return num_list
+
+sorted_list = bubble_sort(unsorted)
+print(sorted_list)
+
+
